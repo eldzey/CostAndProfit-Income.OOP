95d7358bd32edfbfb14beac88f9a74664b663b9d
LJ Hidalgo <129856324+eldzey@users.noreply.github.com>
2023-07-14 21:30:08 +0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,67 +1,90 @@
 ﻿using System;
-using System.Collection.Generic;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections.Generic;
 
-namespace CLASS
+class Program
 {
-    class Product
+    static void Main(string[] args)
     {
-        public string Name { get; }
-        public double Cost { get; }
-        public double SellingPrice { get; }
-
-        public Product(string name, double cost, double SellingPrice)
+        while (true)
         {
-            Name = name;
-            Cost = cost;
-            SellingPrice = sellingPrice;
-        }
-
-        public double CalculateExpenses(int quantity)
-        {
-            return quantity * Cost;
-        }
-
-        public double CalculateSales(int quantity)
-        {
-            return quantity * SellingPrice;
-        }
-
-        public double CalculateIncome(int quantity)
-        {
-            double totalSales = CalculateSales(quantity);
-            double totalExpenses = CalculateExpenses(quantity);
-            return totalSales - totalExpenses;
+            Console.WriteLine("\n-------------------------------------- ");
+            Console.WriteLine("\nChoose a product to compute sales. ");
+            Console.WriteLine("Enter 1: Fries ");
+            Console.WriteLine("Enter 2: Potato Roll ");
+            Console.WriteLine("Enter 3: Cheese Stick ");
+            Console.WriteLine("Enter 4 to arrange the order of income. ");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            if (choice == 4)
+            {
+            Console.WriteLine("\nSuccessfully left the product choices. ");
+            ArrangeOrderOfIncome();
+            return;
+            }
+            CalculateProductSales(choice);
         }
     }
 
-    class CostAndProfit
+    static void CalculateProductSales(int choice)
     {
-        static void Main(string[] args)
+        Product product = GetProduct(choice);
+        Console.WriteLine(product.Name);
+
+
+        Console.Write("Enter the number of product: ");
+        int numOfProduct = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter the cost of the product: ");
+        double productCost = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Enter the selling price: ");
+        double sellingPrice = Convert.ToDouble(Console.ReadLine());
+
+        double totalExpenses =numOfProduct * productCost;
+        double totalSales = numOfProduct * sellingPrice;
+        double totalIncome = totalSales - totalExpenses;
+
+        Console.WriteLine("Total Expenses: Php " + totalExpenses);
+        Console.WriteLine("Total Revenue: Php " + totalSales);
+        Console.WriteLine("\nTotal Profit: Php " + totalIncome);
+
+        if(totalSales > totalExpenses)
+        Console.WriteLine("You have earned net income. ");
+        else
+        Console.WriteLine("This product sales reuslt in a loss. ");
+    }
+
+    static Product GetProduct(int choice)
+    {
+        switch (choice)
         {
-            while (true)
-            {
-                Console.WriteLine()
-                int choice = Convert.ToInt32(Console.ReadLine());
+            case 1:
+            return new Product("\n----------------FRIES----------------IES", 0.0, 0.0);
+            case 2:
+            return new Product("\n--------------POTATO ROLL--------------", 0.0, 0.0);
+            case 3:
+            return new Product("\n-------------CHEESE STICK------------", 0.0, 0.0);
+            default:
+            return null;
+        }
+    }
 
-                if (choice == 4)
-                {
-                    //Console.WriteLine();
+    static void ArrangeOrderOfIncome()
+    {
+        List<string> income = new List<string>();
+        Console.WriteLine("Type the value of total income and name of product then type 'done' to arrange the order of profits. ");
+        Console.WriteLine("*Use NUMBER and SMALL LETTER only* ");
+        string incomeInput = Console.ReadLine();
 
-                    ArrangeOrderOfIncome();
-                    return;
-                }
-                CalculateProductSales(choice);
-            }
+        while (incomeInput != "done")
+        {
+            income.Add(incomeInput);
+            incomeInput = Console.ReadLine();
         }
 
-        static void CalculateProductSales(int choice)
+        Console.WriteLine("\nCongratulations, this is the list of products from most profitable to the least profitable. ");
+        income.Reverse();
 
-        static Product GetProduct(int choice)
-
-        static void ArrangeOrderOfIncome()
-
-
+        foreach (string item in income)
+        {
+            Console.WriteLine(item);
+        }
     }
 }
